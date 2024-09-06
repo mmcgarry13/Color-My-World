c81cbb2a7fde7999001f17fcbd05cdb4e589dc51
josh-hensley <me@joshhensley.com>
2024-09-05 23:53:11 -0500

    random button now populates the hexcode input box

--- assets/script.js
+++ assets/script.js
@@ -14,5 +14,18 @@ function submitHex(e){
 }
 
 function generateRandom(){
+    let r = Math.floor(Math.random() * 255).toString(16);
+    let g = Math.floor(Math.random() * 255).toString(16);
+    let b = Math.floor(Math.random() * 255).toString(16);
+    if (r.length == 1){
+        r = `0${r}`;
+    }
+    if (g.length == 1){
+        g = `0${g}`;
+    }
+    if (b.length == 1){
+        b = `0${b}`;
+    }
 
+    hexcodeInput.value = `#${r}${g}${b}`
 }
